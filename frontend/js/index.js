5e0ad262c75341baf7b711b0a86186749cafb5c4
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-09-01 22:06:15 +0300

    Basic round logic

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,6 +3,78 @@ async function startNewGame() {
   // Creating user or choosing an existing one
   const playerName = await createPlayer();
 
+  // Alert a message with a rules
+  alert('"Who Wants to Be a Programmer?" is a thrilling game where players test their coding knowledge and ' +
+    'problem-solving skills. Contestants face a series of progressively challenging coding questions, each ' +
+    'offering multiple-choice answers. To win the game and claim your first offer as a programmer, players ' +
+    'must answer all questions correctly, with each correct answer bringing them closer to mastering the art of coding.');
+
+  // After player press ok start a new round
+
+  // If it is successful move to a next step, otherwise update user score and exit the program
+
+  // If user passed all questions show success message  and update user score
+}
+// GAME LOGIC
+// Start round
+function startNewRound(number) {
+  const roundTopics = {
+    1: ['html', 'easy'],
+    2: ['html', 'medium'],
+    3: ['html', 'hard'],
+    4: ['css', 'easy'],
+    5: ['css', 'medium'],
+    6: ['css', 'hard'],
+    7: ['js', 'easy'],
+    8: ['js', 'medium'],
+    9: ['js', 'hard'],
+  }
+
+}
+
+// Check round topic by number
+function getRoundTopic(number) {
+  if (number >= 13) {
+    return 'http';
+  } else if (number >= 10 || number < 13) {
+    return 'react';
+  } else if (number >= 7 || number < 10) {
+    return 'javascript';
+  } else if (number >= 4 || number < 7) {
+    return 'css';
+  } else {
+    return 'html';
+  }
+}
+
+function getRoundDifficulty(number) {
+  if (number % 4 === 0) {
+    return 'easy';
+  }
+  else if (number % 3 === 0) {
+    return 'hard';
+  } else if (number % 2 === 0) {
+    return 'medium';
+  } else {
+    return 'easy';
+  }
+}
+
+// Check round topic by number
+function displayQuestion(question) {
+  const questionParagraph = document.querySelector('.question')
+  questionParagraph.textContent = question;
+}
+
+function displayAnswers(answers) {
+  const firstAnswerButton = document.querySelector('.first-answer')
+  const secondAnswerButton = document.querySelector('.first-answer')
+  const thirdAnswerButton = document.querySelector('.first-answer')
+  const fourthAnswerButton = document.querySelector('.first-answer')
+  firstAnswerButton.textContent = answers[0];
+  secondAnswerButton.textContent = answers[1];
+  thirdAnswerButton.textContent = answers[2];
+  fourthAnswerButton.textContent = answers[3];
 }
 
 // MODAL WINDOW CONTROL
@@ -56,20 +128,7 @@ function isPlayerExist(playerName) {
   return !!(playerNames.includes(playerName));
 }
 
-
-
 startNewGame();
-// Fetch player's data or create a new one: (check if it already exists, then ask for a new one)
-
-// Alert a message with a rules
-
-// After player press ok start a new round
-
-// If it is successful move to a next step, otherwise update user score and exit the program
-
-// If user passed all questions show success message  and update user score
-
-
 
 // API FETCH FUNCTIONS
 async function fetchData(requestString) {
@@ -77,18 +136,18 @@ async function fetchData(requestString) {
     .then((data) => data)
     .catch(error => console.log(error));
 }
-async function getPlayerInfo(playerName) {
+async function fetchPlayerInfo(playerName) {
   return await fetchData('Request string'); // !!!! ADD QUERY STRING!!!!!
 }
 
-async function getAllPlayers(playerName) {
+async function fetchAllPlayers(playerName) {
   return await fetchData('Request string'); // !!!! ADD QUERY STRING!!!!!
 }
 
-async function getQuestion(topic, difficulty) {
+async function fetchQuestionAndAnswers(topic, difficulty) {
   return await fetchData('Request string'); // !!!! ADD QUERY STRING!!!!!
 }
 
-async function setNewPlayer(playerName) {
+async function postNewPlayer(playerName) {
   return await fetchData('Request string'); // !!!! ADD QUERY STRING!!!!!
 }
