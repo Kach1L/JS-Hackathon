41c29bb55e416f3422ce982ff301e1e6d9558643
Kach1L <138473110+Kach1L@users.noreply.github.com>
2023-09-01 15:11:04 +0300

    Add files via upload

--- myproject/myapp/serializers.py
+++ myproject/myapp/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
-from .models import Option, Extra
+from .models import Option, Player
 
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
-        fields = ['id','correct_answer', 'wrong_answer1', 'wrong_answer2', 'wrong_answer3']
+        fields = ['id', 'question', 'correct_answer', 'wrong_answer1', 'wrong_answer2', 'wrong_answer3', 'category', 'difficulty']
+        
 
-class ExtraSerializer(serializers.ModelSerializer):
+class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Extra
-        fields = ['id','hint','button','timer']
+        model = Player
+        fields = ['id', 'user_name', 'score']
+
+# class ExtraSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Extra
+#         fields = ['id','hint','button','timer']
--- myproject/myapp/static/script.js
+++ myproject/myapp/static/script.js
@@ -1,30 +1,30 @@
-// const data = {
-//   title: "ddd",
-//   author: "ddd",
-//   publication_date: "2002-05-01",
-//   price: 100,
-// };
-
-// fetch("http://localhost:8000/books/create/", {
-//   method: "POST",
-//   headers: {
-//     "Content-type": "application/json",
-//   },
-//   body: JSON.stringify(data),
-// })
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
-
-fetch("http://localhost:8000/books/all/")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
+const data = {
+    correct_answer: "xxx",
+    wrong_answer1: "xxx",
+    wrong_answer2: "xxx",
+    wrong_answer3: "xxx",
+  };
+  
+  fetch("http://localhost:8000/options/create/", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
   })
-  .catch((e) => {
-    console.log(e);
-  });
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  
+  fetch("http://localhost:8000/options/all/")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
