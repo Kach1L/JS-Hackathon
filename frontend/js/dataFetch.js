9e933a93b159a5abd65ba304bc4c36381ee748ff
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-09-03 12:02:41 +0300

    Improvements for player name and data fetch

--- frontend/js/dataFetch.js
+++ frontend/js/dataFetch.js
@@ -1,21 +1,48 @@
 // API FETCH FUNCTIONS
 async function fetchData(requestString) {
-  return await fetch(requestString)
-    .then((data) => data)
-    .catch(error => console.log(error));
+  try{
+    const data =  await fetch(requestString); // !!!! ADD QUERY STRING!!!!!
+    if (!data.ok) {
+      throw new Error("Corrupted Data")
+    } else{
+      const info = await data.json();
+      console.log(info);
+    }
+  } catch(err){
+    console.log("IN THE CATCH ", err);
+  }}
+
+async function fetchAllPlayers() {
+  fetchData('http://127.0.0.1:8000/players/all/')
 }
+// fetchAllPlayers()
+
+async function fetchQuestionAndAnswers(roundNumber) {
+  fetchData(`http://127.0.0.1:8000/options/${roundNumber}/`)
+}
+
+function postNewPlayer(playerName) {
+  const data = {
+    user_name: playerName,
+    score: 0,
+  };
+
+  fetch("http://localhost:8000/players/create/", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
 async function fetchPlayerInfo(playerName) {
-  return await fetchData('Request string'); // !!!! ADD QUERY STRING!!!!!
-}
-
-async function fetchAllPlayers(playerName) {
-  return await fetchData('Request string'); // !!!! ADD QUERY STRING!!!!!
-}
-
-async function fetchQuestionAndAnswers(topic, difficulty) {
-  return await fetchData('Request string'); // !!!! ADD QUERY STRING!!!!!
-}
-
-async function postNewPlayer(playerName) {
-  return await fetchData('Request string'); // !!!! ADD QUERY STRING!!!!!
+  fetchData(`http://127.0.0.1:8000/players/all/`)
 }
